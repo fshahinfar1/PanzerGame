90d9eb55592bc0d5a24179112dfe73a3b61b1dbd
FSh <fsh1.dev@gmail.com>
2017-01-17 21:05:37 +0330

    feature: LaserBullet

--- fire_load.py
+++ fire_load.py
@@ -6,8 +6,8 @@ import position_class
 import collision_tools
 import wall_obj
 import timer_obj
-from math import radians, sin, cos
-from my_pygame_tools import reflect, calculate_directional_position
+from math import degrees,radians, sin, cos, atan2
+from my_pygame_tools import reflect, calculate_directional_position, distance
 from random import randrange
 FireLoadObjectsList = []
 
@@ -48,18 +48,26 @@ class BulletBase(object):
         if self.room is not None:
             if self.room.is_out_of_room(self.position):
                 self.destroy()
+                return True  # destroy happend
+        return False
+
+    def set_direction(self, value):
+        self.direction = value
+        self.image = pygame.transform.scale(self.image_original, self.size)
+        self.image = pygame.transform.rotozoom(self.image, self.direction, 1)
 
     def loop(self):
         if self.timer.is_time():
             self.destroy()
             return
         # all collision logic of the bullet is here
-        self.update_position()
+        if self.update_position():
+            return
         collide_object = collision_tools.is_colliding(self.collision_obj, self.position, self.collision_obj)
         if collide_object is not None:
             if isinstance(collide_object.get_parent(), wall_obj.Wall):
                 self.position = self.collision_obj.move_to_edge(collide_object, self.direction)
-                self.direction = reflect(self.direction, collide_object.get_colliding_surface_angle(self.collision_obj))
+                self.set_direction(reflect(self.direction, collide_object.get_colliding_surface_angle(self.collision_obj)))
             elif collide_object.is_solid():
                 self.destroy()
                 print('{0} destroy by {1}'.format(self, collide_object))
@@ -164,36 +172,127 @@ class Laser(object):
         LaserBullet(self.fire_position, self.fire_direction, room=room)
 
     def loop(self):
-        if self.fire_state == 'Fire':
-            self.set_position(calculate_directional_position(self.fire_direction, self.fire_position, 10))
+        pass
 
     def draw(self, screen):
         red = (150, 0, 0)
         green = (0, 200, 0)
         pygame.draw.aalines(screen, red, False, self.break_points, 3)
-        for pos in self.break_points:
-            pygame.draw.circle(screen, green, pos.int_cordinates(), 2)
+        # for pos in self.break_points:
+        #     pygame.draw.circle(screen, green, pos.int_cordinates(), 2)
 
 
-class LaserBullet:
-    def __init__(self, pos, direction, speed=20, room=None):
-        self.size = (16, 8)
-        self.image_original = pygame.image.load("./images/laser_bullet.png")
-        # todo it is a good idea to create a class able to handle animation and use it instead
-        self.image = pygame.transform.scale(self.image_original, self.size)
-        self.position = position_class.Position(pos)  # current position of obj
-        self.speed = speed  # moving speed
-        self.direction = direction  # movement direction
-        self.collision_obj = collision_tools.CollisionPoint(self.position, self)
-        # todo Do I really need to give the room to object ??
-        self.room = room  # room which this object is in
-        self.timer = timer_obj.Timer(5)  # 5 sec to self destruction
-        self.timer.set_timer()  # start of timer is creation time
-        FireLoadObjectsList.append(self)  # add self to object list
+class LaserBullet(BulletBase):
+    def __init__(self, pos, direction, speed=3, room=None):
+        size = (2, 2)
+        image = pygame.image.load("./images/laser_bullet.png")
+        collision_obj = collision_tools.CollisionPoint(pos, self)
+        BulletBase.__init__(self, pos, direction, speed, image, size, collision_obj, 500, room)
+        self.collision_points = [pos]
+        self.length = 100
 
-    def destroy(self):
-        self.collision_obj.destroy()
-        FireLoadObjectsList.remove(self)
-        del self  # does it do anything or not??
+    def add_collision_point(self, p):
+        self.collision_points.append(p)
+
+    def loop(self):
+        if self.timer.is_time():
+            self.destroy()
+            return
+        # all collision logic of the bdistance(p, list_temp[0])ullet is here
+        if self.update_position():
+            return
+        collide_object = collision_tools.is_colliding(self.collision_obj, self.position, self.collision_obj)
+        if collide_object is not None:
+            if isinstance(collide_object.get_parent(), wall_obj.Wall):
+                self.position = self.collision_obj.move_to_edge(collide_object, self.direction)
+                self.set_direction(reflect(self.direction, collide_object.get_colliding_surface_angle(self.collision_obj)))
+                self.add_collision_point(self.position)
+            elif collide_object.is_solid():
+                self.destroy()
+                print('{0} destroy by {1}'.format(self, collide_object))
+
+    def line_length(self):
+        list_temp = self.collision_points + [self.position]
+        d = 0
+        for i in range(len(list_temp) - 1):
+            d += distance(list_temp[i], list_temp[i + 1])
+        return d
+
+    def update_collision_points(self):
+        list_temp = self.collision_points + [self.position]
+        d = self.line_length()
+        if d > self.length:
+            len_need = d - self.length
+            p0 = list_temp[0]
+            p1 = list_temp[1]
+            dy = p1[1] - p0[1]
+            dx = p1[0] - p0[0]
+            line_dir = degrees(atan2(dy, dx))
+            p = position_class.Position(calculate_directional_position(line_dir, p0, len_need))
+            if distance(p, list_temp[1]) < 10 and len(list_temp)>2:
+                del self.collision_points[0]
+            else:
+                self.collision_points[0] = p
+
+    def draw(self, screen):
+        blue = (50, 75, 200)
+        self.update_collision_points()
+        screen.blit(self.image, self.get_left_corner())
+        pygame.draw.aalines(screen, blue, False, self.collision_points + [self.position])
+
+
+class WiredLaserBullet(BulletBase):
+    def __init__(self, pos, direction, speed=3, room=None):
+        size = (2, 2)
+        image = pygame.image.load("./images/laser_bullet.png")
+        collision_obj = collision_tools.CollisionPoint(pos, self)
+        BulletBase.__init__(self, pos, direction, speed, image, size, collision_obj, 500, room)
+        self.collision_points = [pos]
+        self.length = 100
+
+    def add_collision_point(self, p):
+        self.collision_points.append(p)
+
+    def loop(self):
+        if self.timer.is_time():
+            self.destroy()
+            return
+        # all collision logic of the bdistance(p, list_temp[0])ullet is here
+        if self.update_position():
+            return
+        collide_object = collision_tools.is_colliding(self.collision_obj, self.position, self.collision_obj)
+        if collide_object is not None:
+            if isinstance(collide_object.get_parent(), wall_obj.Wall):
+                self.position = self.collision_obj.move_to_edge(collide_object, self.direction)
+                self.set_direction(reflect(self.direction, collide_object.get_colliding_surface_angle(self.collision_obj)))
+                self.add_collision_point(self.position)
+            elif collide_object.is_solid():
+                self.destroy()
+                print('{0} destroy by {1}'.format(self, collide_object))
+
+    def update_collision_points(self):
+        list_temp = self.collision_points + [self.position]
+        d = 0
+        for i in range(len(list_temp)-1):
+            d += distance(list_temp[i], list_temp[i+1])
+        if d > self.length:
+            len_need = d - self.length
+            p0 = list_temp[0]
+            p1 = list_temp[1]
+            dy = p1[1] - p0[1]
+            dx = p1[0] - p0[0]
+            line_dir = degrees(atan2(dy, dx))
+            p = position_class.Position(calculate_directional_position(line_dir, p0, len_need))
+            if len(list_temp)>2:
+                del self.collision_points[1]
+            self.collision_points[0] = p
+
+    def draw(self, screen):
+        blue = (50, 75, 200)
+        self.update_collision_points()
+        screen.blit(self.image, self.get_left_corner())
+        pygame.draw.aalines(screen, blue, False, self.collision_points + [self.position])
+
+
 
 
