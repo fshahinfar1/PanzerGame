d3820c772ba2baadcce749fad52a86ed34b2f995
FSh <fsh1.dev@gmail.com>
2017-01-19 12:08:16 +0330

    add destroy class let, the class have its own list of object and Add draw func for player_class

--- player_class.py
+++ player_class.py
@@ -2,16 +2,26 @@
 # Panzer Game
 # 22/10/95
 # player class
+import label_obj
 from my_pygame_tools import KeyboardHandler
 from pygame import joystick
+player_list = []
+
 
 class Player(object):
-    def __init__(self, controller, panzer, key_map):
+    def __init__(self, controller, panzer, key_map, name):
         # if using joystick then it should be supplied if keyboard a keyboard handler should be given
+        self.name = name
         self.controller = controller
         self.key_map = key_map
         self.panzer = panzer
         self.score = 0
+        self.killed = False
+        player_list.append(self)
+
+    def destroy(self):
+        player_list.remove(self)
+        self.panzer.destroy()
 
     def get_panzer(self):
         return self.panzer
@@ -24,3 +34,14 @@ class Player(object):
             return "keyboard"
         elif isinstance(self.controller, joystick.JoystickType):
             return "joystick"
+
+    def add_score(self, value):
+        self.score += value
+
+    def draw(self, screen, pos):
+        label = label_obj.Label(self.name + ": " + str(self.score), pos, (0, 0, 0))
+        label.draw(screen)
+
+
+def clear():
+    player_list.clear()
